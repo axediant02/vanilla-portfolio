0c9e2a148df542210dbdfe2f0f24049fbb343acc
Ian Caballero <ianfredcaballero@gmail.com>
2025-02-06 21:49:22 +0800

    added function for download cv button, and aslo fixed some styling

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log('Hello, world!');
+document.getElementById("download-cv").addEventListener("click", function () {
+    const download = document.createElement("a");
+    download.href = "assets/documents/sample.pdf";
+    download.download = "IanCaballeroCV.pdf";
+
+    document.body.appendChild(download);
+    download.click();
+    document.body.removeChild(download);
+})
